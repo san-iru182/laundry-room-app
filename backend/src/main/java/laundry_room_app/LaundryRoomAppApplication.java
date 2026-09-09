218b179870c474b61e68b888045842cca1810f87
lrod2 <lr72@rice.edu>
2026-09-08 21:11:44 -0500

    Spring init

---
+++ backend/src/main/java/laundry_room_app/LaundryRoomAppApplication.java
@@ -0,0 +1,13 @@
+package laundry_room_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryRoomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryRoomAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/laundry_room_app/LaundryRoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package laundry_room_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryRoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
